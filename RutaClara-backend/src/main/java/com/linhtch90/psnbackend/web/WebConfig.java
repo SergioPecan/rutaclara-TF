8988efe6674cfc70f8eea8a4212dbcf6f0737bf2
Sergio Pecan <u20201a938@upc.edu.pe>
2024-06-22 16:51:14 -0500

    update

--- RutaClara-backend/src/main/java/com/linhtch90/psnbackend/web/WebConfig.java
+++ RutaClara-backend/src/main/java/com/linhtch90/psnbackend/web/WebConfig.java
@@ -3,6 +3,7 @@ package com.linhtch90.psnbackend.web;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,11 +11,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
         Dotenv dotenv = Dotenv.load();
         String clientUrl = dotenv.get("CLIENT_URL");
 
         registry.addMapping("/**")
-                .allowedOrigins(clientUrl,"http://localhost:3000")
+                .allowedOrigins(clientUrl,"http://localhost:3000") // Permite solicitudes CORS desde este origen. Cambia esto a la URL de tu cliente.
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD") // Permite estos métodos.
                 .allowCredentials(true);
     }
