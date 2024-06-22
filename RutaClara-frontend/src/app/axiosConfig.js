4d72fc062b334c6960445cd7ab173dfb0ad5be21
Sergio Pecan <u20201a938@upc.edu.pe>
2024-06-22 18:13:49 -0500

    update

--- RutaClara-frontend/src/app/axiosConfig.js
+++ RutaClara-frontend/src/app/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://rutaclarasocialnetwork-production.up.railway.app',
     headers: {
         'Content-type': 'application/json',
     },
